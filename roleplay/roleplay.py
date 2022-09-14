7ebeb06ad972c67f4649c65bcbccf9d55b94c967
zhengzhi805 <zhengzhi2n2@gmail.com>
2022-09-14 20:05:46 +0800

    rpg revision

--- roleplay/roleplay.py
+++ roleplay/roleplay.py
@@ -10,23 +10,23 @@ class Roleplay(commands.Cog):
 	__author__ = "CafeMeowNeow"
 	__version__ = "0.2.0"
 	
-	default_guild = {"webhook": {}, "characters": {}}
+	default_guild = {"webhooks": {}, "characters": {}}
 	def __init__(self):
 		self.config = Config.get_conf(self, identifier=2817739402)
 		self.config.register_guild(**self.default_guild)
-	
+
 
 	@commands.group()
 	async def roleplay(self, ctx: commands.Context):
-		"""Control your roleplay experience."""
+		"""Control your role-playing experience."""
 
 	@roleplay.group()
 	async def execute(self, ctx: commands.Context):
 		"""Execute user or character."""
-	
+
 	@execute.command(name="char")
-	async def _char_execute(self, ctx: commands.Context, char_id: str, webhook_name: str, *, message):
-		"""Execute someone."""
+	async def _char_execute(self, ctx: commands.Context, char_id: str, webhook_id: str, *, message):
+		"""Execute a character for play."""
 		if char_id is not None:
 			character_dict = await self.config.guild(ctx.guild).characters()
 			char_info = character_dict[char_id]
@@ -38,15 +38,14 @@ class Roleplay(commands.Cog):
 				'username': name,
 				'avatar_url': avatar
 			}
-			webhook_dict = await self.config.guild(ctx.guild).webhook()
+			webhook_dict = await self.config.guild(ctx.guild).webhooks()
 			#this will change to use function link to set webhook_url
-			webhook_url = webhook_dict[webhook_name]
+			webhook_url = webhook_dict[webhook_id]
 			#await ctx.send(payload + webhook_url)
 			send = requests.post(webhook_url, data=payload)
 
-
 	@execute.command()
-	async def member(self, ctx: commands.Context, char_name: discord.Member, webhook_name: str, *, message):
+	async def member(self, ctx: commands.Context, char_name: discord.Member, webhook_id: str, *, message):
 		"""Execute someone."""
 		if char_name is not None:
 			name = char_name.display_name
@@ -56,9 +55,9 @@ class Roleplay(commands.Cog):
 				'username': name,
 				'avatar_url': avatar
 			}
-			webhook_dict = await self.config.guild(ctx.guild).webhook()
+			webhook_dict = await self.config.guild(ctx.guild).webhooks()
 			#this will change to use function link to set webhook_url
-			webhook_url = webhook_dict[webhook_name]
+			webhook_url = webhook_dict[webhook_id]
 			#await ctx.send(payload + webhook_url)
 			send = requests.post(webhook_url, data=payload)
 
@@ -67,10 +66,24 @@ class Roleplay(commands.Cog):
 		"""Manage webhooks."""
 
 	@webhook.command(name="add")
-	async def _add_webhook(self, ctx: commands.Context, webhook_name: str, *, webhook_url: str):
-		webhook_dict = await self.config.guild(ctx.guild).webhook()
-		webhook_dict.update({webhook_name: webhook_url})
-		await self.config.guild(ctx.guild).webhook.set(webhook_dict)
+	async def _add_webhook(self, ctx: commands.Context, webhook_id: str, *, webhook_url: str):
+		"""Add a webhook."""
+		webhook_dict = await self.config.guild(ctx.guild).webhooks()
+		webhook_dict.update({webhook_id: webhook_url})
+		await self.config.guild(ctx.guild).webhooks.set(webhook_dict)
+
+	@webhook.command(name="delete")
+	async def _del_webhook(self, ctx: commands.Context, webhook_id: str):
+		"""Delete a webhook."""
+		webhook_dict = await self.config.guild(ctx.guild).webhooks()
+		del webhook_dict['webhook_id']
+		await self.config.guild(ctx.guild).webhooks.set(webhook_dict)
+
+	@webhook.command(name="list")
+	async def _list_webhook(self, ctx: commands.Context):
+		"""List Webhooks."""
+		webhook_dict = await self.config.guild(ctx.guild).webhooks()
+		await ctx.send(webhook_dict)
 
 	@roleplay.group()
 	async def char(self, ctx: commands.Context):
@@ -83,3 +96,16 @@ class Roleplay(commands.Cog):
 		character_dict.update({char_id: {"username": username, "avatar_url": avatar_url}})
 		await self.config.guild(ctx.guild).characters.set(character_dict)
 		await ctx.send("Character {name} created as {chname}.".format(name=username, chname=char_id))
+
+	@char.command(name="delete")
+	async def _del_char(self, ctx: commands.Context, char_id: str):
+		"""Delete a character."""
+		character_dict = await self.config.guild(ctx.guild).characters()
+		del character_dict['char_id']
+		await self.config.guild(ctx.guild).characters.set(character_dict)
+
+	@char.command(name="list")
+	async def _list_char(self, ctx: commands.Context):
+		"""List characters."""
+		character_dict = await self.config.guild(ctx.guild).characters()
+		await ctx.send(character_dict)
