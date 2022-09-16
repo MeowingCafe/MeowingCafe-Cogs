4ab8727b291588a5a62000800b327936e35f9edf
zhengzhi805 <zhengzhi2n2@gmail.com>
2022-09-16 11:14:28 +0800

    perf

--- roleplay/roleplay.py
+++ roleplay/roleplay.py
@@ -8,17 +8,95 @@ class Roleplay(commands.Cog):
 	"""Use webhook for Role-Playing."""
 	
 	__author__ = "CafeMeowNeow"
-	__version__ = "0.2.0"
+	__version__ = "0.5.0"
 	
 	default_guild = {"webhooks": {}, "characters": {}}
+	default_member = {"interactive_perf": {"status": False}}
 	def __init__(self):
 		self.config = Config.get_conf(self, identifier=2817739402)
 		self.config.register_guild(**self.default_guild)
+		self.config.register_member(**self.default_member)
 
+	async def send(self, guild: commands.Context.guild, author: commands.Context.author, name: str, avatar: str, message: str, webhook_url: str = None):
+		payload = {
+			'content': message,
+			'username': name,
+			'avatar_url': avatar
+		}
+		if webhook_url is None:
+			webhook_dict = await self.config.guild(guild).webhooks()#!
+			perf_dict = await self.config.member(author).interactive_perf()#!
+			webhook_url = perf_dict["webhook"]
+		#await ctx.send(payload + webhook_url)
+		send = requests.post(webhook_url, data=payload)
+
+	async def webhook_check(self, webhook: str):
+		webhook_dict = await self.config.guild(guild).webhooks()#!
+		for webhook_k, webhook_v in webhook_dict:
+			if webhook_k == webhook:
+				return True
+		return False
+
+
+	@commands.Cog.listener()
+	async def on_message(self, message: discord.Message):
+		author = message.author
+		guild = message.guild
+		channel = message.channel.id
+		perf_dict = await self.config.member(author).interactive_perf()
+		if perf_dict["status"] is True:
+			if perf_dict["backstage"] == channel:
+				char_id = perf_dict["char_id"]
+				webhook_id = perf_dict["webhook"]
+				character_dict = await self.config.guild(guild).characters()
+				char_info = character_dict[char_id]
+				await self.send(guild, author, char_info["username"], char_info["avatar_url"], message, webhook_id)
 
 	@commands.group()
 	async def roleplay(self, ctx: commands.Context):
 		"""Control your role-playing experience."""
+
+	@roleplay.command(name="link")
+	async def _link(self, ctx: commands.Context, webhook_id_or_url: str):
+		"""Link a webhook."""
+		is_url = None
+		is_url = re.search(r'^http(s?)://', webhook_id_or_url)
+		perf_dict = await self.config.member(ctx.author).interactive_perf()
+		if is_url:
+			perf_dict.update({"webhook": webhook_id_or_url})
+		else:
+			webhook_dict = await self.config.guild(ctx.guild).webhooks()
+			webhook_url = webhook_dict[webhook_id_or_url]
+			perf_dict.update({"webhook": webhook_url})
+		# await ctx.send(perf_dict)
+		await self.config.member(ctx.author).interactive_perf.set(perf_dict)
+
+	@roleplay.command(name="start")
+	async def _perf_on(self, ctx: commands.Context, char_id: str = None, webhook: str = None, channel: discord.TextChannel = None):
+		"""Start interactive performance."""
+		perf_dict = await self.config.member(ctx.author).interactive_perf()
+		perf_dict.update({"status": True})
+		if char_id is not None:
+			perf_dict.update({"char_id": char_id})
+		if webhook is not None:
+			is_url = None
+			is_url = re.search(r'^http(s?)://', webhook)
+			if is_url:
+				perf_dict.update({"webhook": webhook})
+			else:
+				webhook_dict = await self.config.guild(ctx.guild).webhooks()
+				webhook_url = webhook_dict[webhook]
+				perf_dict.update({"webhook": webhook_url})
+		if channel is not None:
+			perf_dict.update({"backstage": channel.id})
+		await self.config.member(ctx.author).interactive_perf.set(perf_dict)
+
+	@roleplay.command(name="stop")
+	async def _perf_off(self, ctx: commands.Context):
+		"""Stop interactive performance."""
+		perf_dict = await self.config.member(ctx.author).interactive_perf()
+		perf_dict.update({"status": False})
+		await self.config.member(ctx.author).interactive_perf.set(perf_dict)
 
 	@roleplay.group()
 	async def execute(self, ctx: commands.Context):
@@ -27,39 +105,22 @@ class Roleplay(commands.Cog):
 	@execute.command(name="char")
 	async def _char_execute(self, ctx: commands.Context, char_id: str, webhook_id: str, *, message):
 		"""Execute a character for play."""
-		if char_id is not None:
-			character_dict = await self.config.guild(ctx.guild).characters()
-			char_info = character_dict[char_id]
-			#await ctx.send(char_info)
-			name = char_info["username"]
-			avatar = char_info["avatar_url"]
-			payload = {
-				'content': message,
-				'username': name,
-				'avatar_url': avatar
-			}
-			webhook_dict = await self.config.guild(ctx.guild).webhooks()
-			#this will change to use function link to set webhook_url
-			webhook_url = webhook_dict[webhook_id]
-			#await ctx.send(payload + webhook_url)
-			send = requests.post(webhook_url, data=payload)
+		character_dict = await self.config.guild(ctx.guild).characters()
+		char_info = character_dict[char_id]
+		#await ctx.send(char_info)
+		if await webhook_check(webhook_id):
+			await self.send(ctx.guild, ctx.author, char_info["username"], char_info["avatar_url"], message, webhook_id)
+		else:
+			await self.send(ctx.guild, ctx.author, char_info["username"], char_info["avatar_url"], webhook_id + message)
+		
 
 	@execute.command()
-	async def member(self, ctx: commands.Context, char_name: discord.Member, webhook_id: str, *, message):
+	async def member(self, ctx: commands.Context, member_name: discord.Member, webhook_id: str, *, message):
 		"""Execute someone."""
-		if char_name is not None:
-			name = char_name.display_name
-			avatar = str(char_name.avatar_url)
-			payload = {
-				'content': message,
-				'username': name,
-				'avatar_url': avatar
-			}
-			webhook_dict = await self.config.guild(ctx.guild).webhooks()
-			#this will change to use function link to set webhook_url
-			webhook_url = webhook_dict[webhook_id]
-			#await ctx.send(payload + webhook_url)
-			send = requests.post(webhook_url, data=payload)
+		if await webhook_check(webhook_id):
+			await self.send(ctx.guild, ctx.author, member_name.display_name, str(member_name.avatar_url), message, webhook_id)
+		else:
+			await self.send(ctx.guild, ctx.author, member_name.display_name, str(member_name.avatar_url), webhook_id + message)
 
 	@roleplay.group()
 	async def webhook(self, ctx: commands.Context):
@@ -76,12 +137,12 @@ class Roleplay(commands.Cog):
 	async def _del_webhook(self, ctx: commands.Context, webhook_id: str):
 		"""Delete a webhook."""
 		webhook_dict = await self.config.guild(ctx.guild).webhooks()
-		del webhook_dict['webhook_id']
+		del webhook_dict[webhook_id]
 		await self.config.guild(ctx.guild).webhooks.set(webhook_dict)
 
 	@webhook.command(name="list")
 	async def _list_webhook(self, ctx: commands.Context):
-		"""List Webhooks."""
+		"""List webhooks."""
 		webhook_dict = await self.config.guild(ctx.guild).webhooks()
 		await ctx.send(webhook_dict)
 
@@ -101,7 +162,7 @@ class Roleplay(commands.Cog):
 	async def _del_char(self, ctx: commands.Context, char_id: str):
 		"""Delete a character."""
 		character_dict = await self.config.guild(ctx.guild).characters()
-		del character_dict['char_id']
+		del character_dict[char_id]
 		await self.config.guild(ctx.guild).characters.set(character_dict)
 
 	@char.command(name="list")
