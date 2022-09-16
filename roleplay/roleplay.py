0f869fc3b09b5b9ff1c99455dd8677699004d51e
zhengzhi805 <zhengzhi2n2@gmail.com>
2022-09-16 15:34:25 +0800

    debug

--- roleplay/roleplay.py
+++ roleplay/roleplay.py
@@ -18,6 +18,7 @@ class Roleplay(commands.Cog):
 		self.config.register_member(**self.default_member)
 
 	async def send(self, ctx: commands.Context, name: str, avatar: str, message: str, webhook: str = None):
+		perf_dict = await self.config.member(ctx.author).interactive_perf()
 		is_url = None
 		is_url = re.search(r'^http(s?)://', webhook)
 		if is_url:
@@ -32,9 +33,6 @@ class Roleplay(commands.Cog):
 			'username': name,
 			'avatar_url': avatar
 		}
-		if webhook_url is None:
-			perf_dict = await self.config.member(ctx.author).interactive_perf()
-			webhook_url = perf_dict["webhook"]
 		#await ctx.send(payload + webhook_url)
 		send = requests.post(webhook_url, data=payload)
 
@@ -52,10 +50,10 @@ class Roleplay(commands.Cog):
 		if perf_dict["status"] is True:
 			if perf_dict["backstage"] == ctx.channel:
 				char_id = perf_dict["char_id"]
-				webhook_id = perf_dict["webhook"]
+				webhook = perf_dict["webhook"]
 				character_dict = await self.config.guild(ctx.guild).characters()
 				char_info = character_dict[char_id]
-				await self.send(ctx, char_info["username"], char_info["avatar_url"], message.content, webhook_id)
+				await self.send(ctx, char_info["username"], char_info["avatar_url"], ctx.content, webhook)
 
 	@commands.group()
 	async def roleplay(self, ctx):
@@ -96,23 +94,23 @@ class Roleplay(commands.Cog):
 		await self.config.member(ctx.author).interactive_perf.set(perf_dict)
 
 	@roleplay.command(name="cast")
-	async def _char_execute(self, ctx: commands.Context, char_id: str, webhook_id: str, *, message):
+	async def _char_execute(self, ctx: commands.Context, char_id: str, webhook: str, *, message):
 		"""Cast a character to send message."""
 		character_dict = await self.config.guild(ctx.guild).characters()
 		char_info = character_dict[char_id]
 		#await ctx.send(char_info)
-		if await self.webhook_check(ctx.guild, webhook_id):
-			await self.send(ctx, char_info["username"], char_info["avatar_url"], message, webhook_id)
+		if await self.webhook_check(ctx.guild, webhook):
+			await self.send(ctx, char_info["username"], char_info["avatar_url"], message, webhook)
 		else:
-			await self.send(ctx, char_info["username"], char_info["avatar_url"], webhook_id + message)
+			await self.send(ctx, char_info["username"], char_info["avatar_url"], webhook + message)
 
 	@roleplay.command(name="disg")
-	async def _member_execute(self, ctx: commands.Context, member_name: discord.Member, webhook_id: str, *, message):
+	async def _member_execute(self, ctx: commands.Context, member_name: discord.Member, webhook: str, *, message):
 		"""Disguise a user to send message."""
-		if await self.webhook_check(ctx.guild, webhook_id):
-			await self.send(ctx, member_name.display_name, str(member_name.avatar_url), message, webhook_id)
+		if await self.webhook_check(ctx.guild, webhook):
+			await self.send(ctx, member_name.display_name, str(member_name.avatar_url), message, webhook)
 		else:
-			await self.send(ctx, member_name.display_name, str(member_name.avatar_url), webhook_id + message)
+			await self.send(ctx, member_name.display_name, str(member_name.avatar_url), webhook + message)
 
 	@roleplay.group()
 	async def webhook(self, ctx):
